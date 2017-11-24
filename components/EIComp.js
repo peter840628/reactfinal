302fd4955f28e1c0297b4f005bd1410072dd764a
Peta <peter840628@gmail.com>
2017-11-24 10:03:44 -0800

    simple wireframe of the edit item page with input, lables and buttons

--- components/EIComp.js
+++ components/EIComp.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default class EIComp extends Component {
 
-  static navigationOptions = {
-    title: 'Edit Items',
-  };
+    static navigationOptions = {
+        title: 'Edit Items',
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello Once More, Consumer</Text>
-        <Icon.Button
-        name="rebel"
-        backgroundColor="blue">
-        Destroy the Ottoman Empire
-        </Icon.Button>
+    render() {
+        return (
+            <View style={styles.container}>
+<Text>Please insert the name of the food :</Text>
+<TextInput style={styles.textInput}/>
+<TouchableOpacity style={styles.button}>
+  <Text>Submit</Text>
+</TouchableOpacity>
+
+<TouchableOpacity style={styles.button}>
+  <Text>Reset</Text>
+</TouchableOpacity>
+
+<Text>Please select the picture of the food :</Text>
+
+
+<TouchableOpacity style={styles.button}>
+  <Text>Add Item</Text>
+</TouchableOpacity>
+
       </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+
+
+    },
+    textInput: {
+        marginTop: 10,
+        margin: 10,
+        alignItems: 'flex-start'
+    },
+
+    button: {
+        margin: 5,
+        backgroundColor: '#85c1e9',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+
+}
+);
